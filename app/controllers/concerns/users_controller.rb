69b87ecf37b94680a5abd1e97623f302fac0f413
Rocketman <lakshyavdatta@gmail.com>
2018-11-02 04:25:29 +0000

    Signup users from UI

---
+++ app/controllers/concerns/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "We're glad to have you as the newest member on our team, #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
